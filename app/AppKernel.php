a74c46a78070f4515b8a19ddf721ca5548179ea9
giboow <philippe.gibert@gmail.com>
2013-01-01 22:58:11 +0100

    Disable FOSUserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,7 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
         	new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
-        	new FOS\UserBundle\FOSUserBundle(),
+        	//new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
