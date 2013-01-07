23e480bcfe0d3ee7d9004c2e93a48d382c21ca90
giboow <philippe.gibert@gmail.com>
2013-01-08 00:09:07 +0100

    update capifony config

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -3,6 +3,10 @@ set :domain,      "#{application}.me"
 set :deploy_to,   "/var/www/#{domain}"
 set :app_path,    "app"
 
+
+set :user, "root"
+
+
 set :repository,  "git@github.com:giboow/DrinkWith.git"
 set :scm,         :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
@@ -10,11 +14,18 @@ set :scm,         :git
 set :model_manager, "doctrine"
 # Or: `propel`
 
+
+
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain                         # This may be the same as your `Web` server
 role :db,         domain, :primary => true       # This is where Symfony2 migrations will run
 
+set :shared_children,     ["logs", "web/uploads", "vendor"]
+
 set  :keep_releases,  3
+set :dump_assetic_assets, true
+set :use_composer, true
+set :update_vendors, true
 
 # Be more verbose by uncommenting the following line
-# logger.level = Logger::MAX_LEVEL
+logger.level = Logger::MAX_LEVEL
