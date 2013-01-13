4b7e88b5da2fd3f47f125af9c4533b3a52d68cae
giboow <philippe.gibert@gmail.com>
2013-01-13 14:01:08 +0100

    Replace php translation for DrinkWithMainBundel for yaml files

--- src/DrinkWith/Bundle/MainBundle/Resources/translations/messages.fr.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array(
-        "contact.us"    => "Contactez nous",
-        "login" => "Se connecter",
-        "subscribe" => "S'inscrire",
-        "home" => "Accueil",
-        "hello" => "Bonjour %name%"
-);
