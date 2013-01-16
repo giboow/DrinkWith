a93285dcec114183725fe5b3303472a40bf16298
giboow <philippe.gibert@gmail.com>
2013-01-16 22:29:11 +0100

    debug

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -11,14 +11,15 @@ set :app_path,    "app"
 
 
 set :user, "jenkins"
-set :use_sudo, true
+set :use_sudo, false
 set :group_writable, true
 set :writable_dirs,     ["app/cache", "app/logs"]
 set :webserver_user,    "www-data"
+set :permission_method, :chown
+#set :use_set_permissions, true
 
 set :repository,  "git@github.com:giboow/DrinkWith.git"
 set :scm,         :git
-set :deploy_via,  :rsync_with_remote_cache
 
 set :model_manager, "doctrine"
 
@@ -26,13 +27,12 @@ role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain                         # This may be the same as your `Web` server
 role :db,         domain, :primary => true       # This is where Symfony2 migrations will run
 
-set  :shared_children,     ["logs", "web/uploads", "vendor"]
+set :shared_children,     ["logs", "web/uploads", "vendor"]
 
 set  :keep_releases,  3
-set  :dump_assetic_assets, true
-set  :use_composer, true
-set  :update_vendors, true
-
+set :dump_assetic_assets, true
+set :use_composer, true
+set :update_vendors, true
 
 # Be more verbose by uncommenting the following line
 #logger.level = Logger::MAX_LEVEL
